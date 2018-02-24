61cd5bec0cd98de2e88e9fdd0cfb9be07c7c36be
David Wolff <davidwolff2020@u.northwestern.edu>
2018-02-24 02:38:31 -0600

    Create matching program that outputs array of answers

--- public/scripts/matching.js
+++ public/scripts/matching.js
@@ -1,3 +1,28 @@
-function matching(input) {
-  console.log("MATCH ME BRO");
+function matching(answers, locations) {
+  var matchOutput = [];
+  var location, matchingstrings, best;
+  var bestcount = 0;
+  if(answers.length !== locations.keys[0].length) {
+    console.log('Something wrong with answers and location array.');
+  } else {
+    locations.keys.forEach((index) => {
+      count = 0;
+      location = locations.keys[index];
+      matchingstrings = locations[index];
+      matchingstrings.forEach((answer) => {
+        if(answers.indexOf(answer) !== -1) {
+          count++;
+        }
+      })
+      if(count > bestcount) {
+        matchOutput = [];
+        matchOutput.push(location);
+      }
+      if(count === bestcount) {
+        matchOutput.push(location);
+      }
+    })
+  }
+
+  return matchOutput;
 }
